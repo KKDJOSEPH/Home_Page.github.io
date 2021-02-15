002069942dfe788388716decdb60c147a9f0538b
KKDJOSEPH <kkdjoseph@163.com>
2021-02-15 14:38:30 -0800

    update js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 window.addEventListener('load',function(){
-    let imgSrcs = ['./assets/gallery/1.JPG','./assets/gallery/2.JPG','./assets/gallery/4.JPG','./assets/gallery/5.JPG',
-        './assets/gallery/6.JPG','./assets/gallery/7.JPG','./assets/gallery/8.JPG','./assets/gallery/10.JPG','./assets/gallery/11.JPG'];
+    let imgSrcs = ['./assets/gallery/1.JPG', './assets/gallery/2.JPG', './assets/gallery/4.JPG', './assets/gallery/5.JPG',
+        './assets/gallery/6.JPG', './assets/gallery/7.JPG', './assets/gallery/8.JPG', './assets/gallery/10.JPG', './assets/gallery/11.JPG'];
     let interval = 1000;
     let viewer = document.getElementById('viewer');
     let current = 0;
     let len = imgSrcs.length;
     let setImage = function(){
         viewer.src = imgSrcs[current];
-        current = ++current>len-1? 0 : current;
+        current = ++current > len-1 ? 0 : current;
     };
     setImage();
-    setInterval(setImage,interval);
+    setInterval(setImage, interval);
 });
