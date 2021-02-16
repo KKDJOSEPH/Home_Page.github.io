deb6b30186be90e619e528eb3bf321c7e2ff2d59
KKDJOSEPH <kkdjoseph@163.com>
2021-02-16 14:24:34 -0800

    repackage the code, change the way of using eslint for future use

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,16 @@
-window.addEventListener('load',function(){
-    let imgSrcs = ['./assets/gallery/1.JPG', './assets/gallery/2.JPG', './assets/gallery/4.JPG', './assets/gallery/5.JPG',
-        './assets/gallery/6.JPG', './assets/gallery/7.JPG', './assets/gallery/8.JPG', './assets/gallery/10.JPG', './assets/gallery/11.JPG'];
-    let interval = 1000;
-    let viewer = document.getElementById('viewer');
-    let current = 0;
-    let len = imgSrcs.length;
-    let setImage = function(){
-        viewer.src = imgSrcs[current];
-        current = ++current > len-1 ? 0 : current;
-    };
-    setImage();
-    setInterval(setImage, interval);
+window.addEventListener("load",function(){
+  let imgSrcs = ["./assets/gallery/1.JPG", "./assets/gallery/2.JPG", "./assets/gallery/4.JPG", "./assets/gallery/5.JPG", "./assets/gallery/6.JPG", "./assets/gallery/7.JPG", "./assets/gallery/8.JPG", "./assets/gallery/10.JPG", "./assets/gallery/11.JPG"];
+  // eslint-disable-next-line no-irregular-whitespace
+  let interval = 1000;
+  // eslint-disable-next-line no-irregular-whitespace
+  let viewer = document.getElementById("viewer");
+  let current = 0;
+  // eslint-disable-next-line indent
+  let len = imgSrcs.length;
+  let setImage = function(){
+    viewer.src = imgSrcs[current];
+    current = ++current > len-1 ? 0 : current;
+  };
+  setImage();
+  setInterval(setImage, interval);
 });
